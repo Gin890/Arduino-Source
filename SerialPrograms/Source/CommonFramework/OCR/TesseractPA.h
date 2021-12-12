0c0a6a8956aa5dbffc3a4785057058e89959d9dc
Gin <>
2021-12-12 10:58:40 -0800

    Add dependency on Tesseract for unix

--- SerialPrograms/Source/CommonFramework/OCR/TesseractPA.h
+++ SerialPrograms/Source/CommonFramework/OCR/TesseractPA.h
@@ -21,22 +21,18 @@
 #define TESSERACT_EXPORT __declspec(dllexport)
 #else
 #define TESSERACT_EXPORT __declspec(dllimport)
-#endif
-
-#else
-
-#define TESSERACT_EXPORT __attribute__((visibility("default")))
-
-#endif
-#endif
-
+#endif // end for _WINDLL
 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
+// We build a custom Tesseract library,
+// https://github.com/PokemonAutomation/Tesseract-OCR_for_Windows
+// to ship with SerialProgram on Windows, in the form of tesseractPA.dll.
+// The following C API is a wrapper for the raw Tesseract API. The wrapper
+// along with Tesseract itself is implemented in tesseractPA.dll.
 struct TesseractAPI_internal;
 TESSERACT_EXPORT TesseractAPI_internal* TesseractAPI_construct(
     const char* path, const char* language
@@ -58,7 +54,61 @@ TESSERACT_EXPORT void Tesseract_delete(char* text);
 }
 #endif
 
+// end for _WIN32
+#elif defined(UNIX_LINK_TESSERACT)
 
+// We hope to use our own Tesseract build in future for Unix systems too.
+// But right now to run on Linux and Mac we need to use external Tesseract library API directly.
+// So the Tesseract API wrapper is defined here to match Windows.
+
+#include <tesseract/baseapi.h>
+#include <iostream>
+
+struct TesseractAPI_internal{
+    tesseract::TessBaseAPI m_api;
+
+    TesseractAPI_internal(const char* path, const char* language){
+        if (m_api.Init(path, language)){
+            throw "Could not initialize TesseractAPI.";
+        }
+    }
+};
+
+inline TesseractAPI_internal* TesseractAPI_construct(const char* path, const char* language){
+    try{
+        return new TesseractAPI_internal(path, language);
+    }catch (const char* err){
+        std::cout << err << std::endl;
+    }
+    return nullptr;
+}
+
+inline void TesseractAPI_destroy(TesseractAPI_internal* api){
+    delete api;
+}
+
+inline char* TesseractAPI_read_bitmap(
+    TesseractAPI_internal* api,
+    const unsigned char* data,
+    size_t width, size_t height,
+    size_t bytes_per_pixel, size_t bytes_per_line,
+    size_t ppi
+){
+    api->m_api.SetImage(data, (int)width, (int)height, (int)bytes_per_pixel, (int)bytes_per_line);
+    api->m_api.SetSourceResolution((int)ppi);
+    return api->m_api.GetUTF8Text();
+}
+inline void Tesseract_delete(char* text){
+    delete[] text;
+}
+
+// end for UNIX_LINK_TESSERACT
+#else
+
+// #define TESSERACT_EXPORT __attribute__((visibility("default")))
+
+#endif 
+#endif // end for TESSERACT_STATIC
 
 
 class TesseractString{
@@ -92,7 +142,6 @@ private:
         : m_str(str)
     {}
 
-private:
     friend class TesseractAPI;
     char* m_str;
 };
